62a95d7dd2588bf18090c14e602993c64ac01393
haridivya <123063502+haridivya@users.noreply.github.com>
2024-09-21 22:58:37 +0530

    FeeAmount.py

--- FeeAmount.py
+++ FeeAmount.py
@@ -5,17 +5,7 @@ Refer the table below for fee details.
 Merit Seat Day Scholar                  MSDS                  Tuition fee + Bus fee
 Merit Seat Hosteller                    MSH                   Tuition fee + Hostel fee
 Management Seat Day Scholar            MGSDS                  150% of Tuition fee + Bus fee
-
-
-
-Management Seat Hosteller
-MGSH
-
-150% of Tuition fee + Hostel fee
-
-
-
-
+Management Seat Hosteller              MGSH                   150% of Tuition fee + Hostel fee
 Input format:
 The first input corresponds to the student type
 The second input corresponds to the tuition fee
@@ -29,3 +19,4 @@ MSH
 50000
 Sample Output:
 90000.00
+'''
