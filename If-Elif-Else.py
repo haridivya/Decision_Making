f16fd43d9182dba7a98c8685d0dc51c4cff7abca
haridivya <123063502+haridivya@users.noreply.github.com>
2024-09-24 20:00:30 +0530

    Update If-Elif-Else.py

--- If-Elif-Else.py
+++ If-Elif-Else.py
@@ -8,7 +8,15 @@ The output consists of a below-given string
 “Vowel” / “Consonant” / “Not an alphabet”
 Sample Input:
 e
-
 Sample Output:
 Vowel
 '''
+element=input()
+vowels=['a','e','i','o','u','A','E','I','O','U']
+if element.isalpha():
+    if element in vowels:
+        print("Vowel")
+    else:
+        print('Consonant')
+else:
+    print('Not an alphabet')
