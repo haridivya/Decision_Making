c9f8b5f220cf67d1a25d39431afd0ce1e6f93217
haridivya <123063502+haridivya@users.noreply.github.com>
2024-09-24 22:25:21 +0530

    Update labs3.py

--- labs3.py
+++ labs3.py
@@ -1,3 +1,4 @@
+
 '''
 There are 3 labs in the CSE department are L1, L2, and L3 with a seating capacity of x, y, and z respectively. One of the 3 labs has been allocated for ACE training. Out of the available labs, find the lab which has minimal seating capacity.
 Input format:
@@ -17,3 +18,23 @@ L3
 Sample Output:
 L1
 '''
+l1=int(input())
+l2=int(input())
+l3=int(input())
+val=input()
+if 'l1'==val:
+    if l2<l3:
+        print(l2)
+    else:
+        print(l3)
+elif 'l2'==val:
+    if l1<l3:
+        print(l1)
+    else:
+        print(l3)
+elif 'l3' == val:
+    if l1<l2:
+        print(l1)
+    else:
+        print(l2)
+
